419b49fb649d6014b068e5038a4f856494758ac7
slawyn <unixtracer@gmail.com>
2024-12-09 21:45:15 +0100

    adds functions to main

--- bomberman/app/src/jni/code/GameLogic.h
+++ bomberman/app/src/jni/code/GameLogic.h
@@ -5,25 +5,24 @@
 extern "C" {
 
 
-#define     OBJ_BOMB  0x1000
-#define     OBJ_EXPLN  0x2000
-#define     OBJ_CRATE  0x3000
-#define     OBJ_BLOCK  0x4000
-#define     OBJ_ITEM  0x5000
-#define     OBJ_PLAYR  0x6000
+#define OBJ_MASK  0xF000
+#define OBJ_BOMB  0x1000
+#define OBJ_EXPLN 0x2000
+#define OBJ_CRATE 0x3000
+#define OBJ_BLOCK 0x4000
+#define OBJ_ITEM  0x5000
+#define OBJ_PLAYR 0x6000
 
-
-#define OBJ_MASK                    (0xF000)
 void vGameGetFieldSizes(int32_t ri32FieldSizes[2]);
-int32_t jiUpdateObjects(int32_t ji32Dt, uint32_t *ui32PlayerUpdates);
-int32_t  jiGameGetObjects(int32_t ** ppui32Objects);
-int32_t  jiGameGetRemovedObjects(int32_t ** ppui32Objects);
-int32_t jiGameCreate();
-int16_t i16GameGetObjectZ(int32_t i32ObjType, int32_t i32ObjectStateOffset);
-int32_t jiGameUpdateTicker();
-uint32_t ui32GameGetState(int32_t i32ObjType, int32_t i32ObjectStateOffset);
-int32_t jiGameGetPositions(float ** ppfPositions, int32_t i32ObjType, int32_t i32ObjectStateOffset);
 void vGameSetInput(int32_t i32ObjectStateOffset, uint8_t ui8Input);
+int32_t jiUpdateObjects(int32_t ji32Dt, uint32_t *ui32PlayerUpdates);
+int32_t jiGameGetObjects(int32_t ** ppui32Objects);
+int32_t jiGameGetRemovedObjects(int32_t ** ppui32Objects);
+int16_t i16GameGetObjectZ(int32_t i32ObjType, int32_t i32ObjectStateOffset);
+int32_t jiGameGetPositions(float ** ppfPositions, int32_t i32ObjType, int32_t i32ObjectStateOffset);
 int32_t jiGameGetHitbox(int32_t **ppi32Hitbox, int32_t i32ObjType, int32_t i32ObjectStateOffset);
+uint32_t ui32GameGetState(int32_t i32ObjType, int32_t i32ObjectStateOffset);
+int32_t jiGameCreate();
+int32_t jiGameUpdateTicker();
 }
 #endif
--- bomberman/app/src/jni/main.cpp
+++ bomberman/app/src/jni/main.cpp
@@ -1,11 +1,60 @@
+#define _HAS_STD_BYTE 0
+#define NOMINMAX
+#include <Windows.h>
+
 using namespace std;
 #include <iostream>
-
 #include "GameLogic.h"
+
+int32_t ri32FieldSizes[2u];
+bool bGameExecuting = true;
+
+DWORD WINAPI thread1(LPVOID pm)
+{   (void)pm;
+    //check the getchar value
+    int a = getchar();
+    while (a != '0'){
+        a = getchar();
+    }
+    bGameExecuting = false;
+    return 0;
+}
+
+int32_t STEP = 20;
+uint8_t ui8Input = 0u;
+
 
 int main(int argc, char** argv){
     (void)argc;
     (void)argv;
-    std::cout<<"Hello"<<std::endl;
+
+    HANDLE handle = CreateThread(NULL, 0, thread1, NULL, 0, NULL);
+    (void) handle;
+
+    vGameGetFieldSizes(ri32FieldSizes);
+    std::cout<<"x:"<<ri32FieldSizes[0]<<" y:"<<ri32FieldSizes[1]<<std::endl;
+    jiGameCreate();
+    uint32_t ui32UpdatedPlayers = 0;
+
+    while(bGameExecuting){
+        int32_t i32ObjectStateOffset = 0;
+        vGameSetInput(i32ObjectStateOffset, ui8Input);
+
+
+        jiUpdateObjects(STEP, &ui32UpdatedPlayers);
+
+        int32_t *pjiObjects = NULL;
+        int16_t i16TotalCount = jiGameGetRemovedObjects(&pjiObjects);
+        (void) i16TotalCount;
+
+
+        // ui32GameGetState(i32ObjType, i32ObjectStateOffset)
+        // jiGameGetPositions(float ** ppfPositions, int32_t i32ObjType, int32_t i32ObjectStateOffset)
+        // jiGameGetHitbox(int32_t **ppi32Hitbox, int32_t i32ObjType, int32_t i32ObjectStateOffset)
+        // jiGameGetObjects(int32_t ** ppui32Objects);
+        // i16GameGetObjectZ(int32_t i32ObjType, int32_t i32ObjectStateOffset);
+        // jiGameUpdateTicker(void)
+    }
+    
     return 0;
 }
